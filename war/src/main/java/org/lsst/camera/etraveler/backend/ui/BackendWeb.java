92aa9f6a4241b63a8a64306164a3a45e06b4bb07
Joanne Bogart <jrb@slac.stanford.edu>
2016-01-28 14:49:59 -0800

    show short description rather than description in traveler type table; fix a reorg bug

--- war/src/main/java/org/lsst/camera/etraveler/backend/ui/BackendWeb.java
+++ war/src/main/java/org/lsst/camera/etraveler/backend/ui/BackendWeb.java
@@ -38,7 +38,7 @@ public class BackendWeb {
     /* Include two fake columns, values to be overwritten by displayTable decorator */
     String[] cols = {"P.name as name", "P.version as version", "HG.name as hname",
       "HG.id as hgid",
-      "P.description as description", "P.createdBy", "P.creationTS", "TTS.name as state", 
+      "P.shortDescription as description", "P.createdBy", "P.creationTS", "TTS.name as state", 
       "'vw' as viewEdit", "'an' as addNCR"};
     String tableSpec = "Process P inner join TravelerType TT on P.id=TT.rootProcessId ";
     tableSpec += "inner join HardwareGroup HG on HG.id=P.hardwareGroupId ";
